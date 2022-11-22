b42252eeed3fe09bfd5b9e38c9a09071a422f0ba
code-sum <yisugyeong@gmail.com>
2022-11-22 22:26:40 +0900

    221122 PRG 풀이

--- Programmers/LV0/분수의덧셈.py
+++ Programmers/LV0/분수의덧셈.py
@@ -1,11 +1,10 @@
+from fractions import Fraction
+
 def solution(denum1, num1, denum2, num2):
-    ans = []
-    if num1//num2 == 0 or num2//num1 == 0:
-        if num1 >= num2:
-            
-    else:
-        deresult = (num2*denum1)+(num1*denum2)
-        ans.append(deresult)
-        result = num1*num2
-        ans.append(result)        
-    return ans
+    answer = Fraction(denum1, num1) + Fraction(denum2, num2)
+    return [answer.numerator, answer.denominator]
+
+# Fraction : 유리수 연산에 쓰임. Fraction(분자, 분모) 는 분자/분모 형태의 유리수
+# 유리수 : 분자와 분모가 서로소이고, 소수에서 소수점 이하의 수가 반복적인 순환소수를 포함하는 수
+# a.numerator : a 라는 분수의 분자 출력
+# a.denominator : a 라는 분수의 분모 출력
